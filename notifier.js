03ac3cf9c40d7e8769d278305e3c3b38a4915ba7
abhimanyu <abhimanyukrsingh00@gmail.com>
2021-05-21 20:27:31 +0530

    Update notifier.js

--- notifier.js
+++ notifier.js
@@ -1,36 +1,35 @@
+function sendBotMessage() {
+      var request = new XMLHttpRequest();
+      request.open("POST", "https://api.telegram.org/botYOUR_TOKEN/sendMessage");
 
+      request.setRequestHeader('Content-type', 'application/json');
+
+      var params = {
+        "chat_id": "YOUR_CHAT_ID",
+        "text": "Vaccine Available!!"
+      }
+
+      request.send(JSON.stringify(params));
+}
 function showNotification() {
-
    const notification = new Notification("New message incoming", {
-
       body: "Hi there. Vaccine is Available!"
-
    })
-
 }
-
 function findAd(){
-
 	document.getElementsByClassName('pin-search-btn')[0].click()
-
 	if (Notification.permission === "granted") {
-
       alert("we have permission");
-
     } else if (Notification.permission !== "denied") {
-
       Notification.requestPermission().then(permission => {
-
          console.log(permission);
-
 		 if(document.getElementsByClassName('slots-box no-available').length < 6 || document.querySelectorAll('a[title="Fully Booked"]') < 1)
-
+		 {
 			showNotification();
-
+			sendBotMessage();
+		 }
+			
       });
-
    }
-
 }
-
-setInterval(findAd,1000);
+setInterval(findAd,60000);
