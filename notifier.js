be6cc43f3af544a3d48235820c4d88347446062d
abhimanyu <abhimanyukrsingh00@gmail.com>
2021-05-21 22:44:32 +0530

    Update notifier.js

--- notifier.js
+++ notifier.js
@@ -11,18 +11,21 @@ function sendBotMessage() {
 
       request.send(JSON.stringify(params));
 }
+
 function showNotification() {
    const notification = new Notification("New message incoming", {
       body: "Hi there. Vaccine is Available!"
    })
 }
+
 function findAd(){
-	document.getElementsByClassName('pin-search-btn')[0].click()
+	document.getElementsByClassName('pin-search-btn')[0].click();
 	if (Notification.permission === "granted") {
-      alert("we have permission");
-    } else if (Notification.permission !== "denied") {
-      Notification.requestPermission().then(permission => {
-         console.log(permission);
+     		 alert("we have permission");
+    	} 
+	else if (Notification.permission !== "denied") {
+     	 	Notification.requestPermission().then(permission => {
+         	console.log(permission);
 		 if(document.getElementsByClassName('slots-box no-available').length < 6 || document.querySelectorAll('a[title="Fully Booked"]') < 1)
 		 {
 			showNotification();
